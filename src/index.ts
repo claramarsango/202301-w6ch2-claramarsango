64f125e99c8abe0c7483ce24811003a62b83f426
claramarsango <claraxmarsango@gmail.com>
2023-02-22 23:19:25 +0100

    create app, port, app router and raise server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export const hello = {
-  hello: 'Hello world',
-};
+import express from 'express';
+import apiRouter from './api/api-router.js';
+
+const app = express();
+const port = 3000;
+
+app.use('/api/v1', apiRouter);
+
+app.listen(port, () => {
+  'hello world';
+});
