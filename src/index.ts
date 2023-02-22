d6e992b8f353672cfe34f3f0eb80345c4647ae7d
claramarsango <claraxmarsango@gmail.com>
2023-02-22 23:38:29 +0100

    add info to root path about other routes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ const port = 3000;
 
 app.use('/api/v1', apiRouter);
 
+app.get('/', (req, res) => {
+  res.json(
+    'Go to "https://two02301-w6ch2-claramarsango.onrender.com/api/v1/things" to see some things I already know',
+  );
+});
+
 app.listen(port, () => {
   'hello world';
 });
